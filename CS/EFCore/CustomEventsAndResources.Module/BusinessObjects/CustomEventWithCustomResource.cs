a59188b4a8d8bf0bcfcd355c24805aec0e16db9e
Svetlana Nikulina <svetlana.nikulina@devexpress.com>
2026-06-05 09:01:53 +0200

    fix build

--- CS/EFCore/CustomEventsAndResources.Module/BusinessObjects/CustomEventWithCustomResource.cs
+++ CS/EFCore/CustomEventsAndResources.Module/BusinessObjects/CustomEventWithCustomResource.cs
@@ -158,7 +158,7 @@ public class CustomEventWithCustomResource : BaseObject, IEvent, IXafEntityObjec
         }
     }
     private IList<PostponeTime> CreatePostponeTimes() {
-        IList<PostponeTime> result = PostponeTime.CreateDefaultPostponeTimesList();
+        List<PostponeTime> result = PostponeTime.CreateDefaultPostponeTimesList();
         result.Add(new PostponeTime("None", null, "None"));
         result.Add(new PostponeTime("AtStartTime", TimeSpan.Zero, "0 minutes"));
         CustomizeNotificationsPostponeTimeListEventArgs args = new CustomizeNotificationsPostponeTimeListEventArgs(result);
